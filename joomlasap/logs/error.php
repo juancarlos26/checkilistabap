55f19e007bd557a47f89f78befbc4636548763f1
juancarlos26 <juancarlos.mr26@gmail.com>
2016-01-26 16:52:12 -0600

    checklistfromcomputer
    checklistfromcomputer

---
+++ joomlasap/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-01-21 22:41:00 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-01-21T22:41:00+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-01-22T20:54:36+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
